16271b37bdc737e5c568b834485a95b58cbd7742
kundanbasha <alamkundan1300@gmail.com>
2017-08-18 15:02:31 +0530

    deleted comment in the itemsactivity.java

--- app/src/main/java/com/example/karunakar/pizzasapp/ItemsActivity.java
+++ app/src/main/java/com/example/karunakar/pizzasapp/ItemsActivity.java
@@ -35,7 +35,7 @@ public class ItemsActivity extends AppCompatActivity {
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //Toast.makeText(ItemsActivity.this, "You Clicked at " +itemnames[position], Toast.LENGTH_SHORT).show();
+
 
                 if(itemnames[position]=="veg"){
 
